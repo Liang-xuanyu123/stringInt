98ccdf3d16af138c4fd5682378c46d10832c4c43
Liang-xuanyu123 <liangxuanyu201603@163.com>
2026-02-26 16:58:05 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,11 +1,9 @@
 #include "stringInt.hpp"
-#include <iostream>
-#include <string.h>
 using namespace std;
 int main()
 {
     sInt a("54088"), b("53341"), c("999");
-    cout<<a+b+c;
+    cout<<a+b+c<<"\n";
+    cout<<b-c<<"\n";
     return 0;
 }
-
